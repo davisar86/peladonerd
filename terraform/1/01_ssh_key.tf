56d50d0f11a1bca82e124706e0c4e6cfbc2197b6
davisar86 <70041180+davisar86@users.noreply.github.com>
2020-08-21 17:35:01 -0600

    Update 01_ssh_key.tf

--- terraform/1/01_ssh_key.tf
+++ terraform/1/01_ssh_key.tf
@@ -1,8 +1,8 @@
 #
 # Exportamos nuestra key SSH
 
-resource "digitalocean_ssh_key" "pelado" {
-  name       = "pelado"
+resource "digitalocean_ssh_key" "salvation" {
+  name       = "salvation"
   public_key = "${file("id_rsa.pub")}"
 }
 
